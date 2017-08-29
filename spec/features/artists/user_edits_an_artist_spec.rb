4e6d421436554b45c12b9809fc56dfef47c08656
Mike Heft <mikeheft@gmail.com>
2017-08-29 17:37:40 -0600

    add edit functionality actions

--- spec/features/artists/user_edits_an_artist_spec.rb
+++ spec/features/artists/user_edits_an_artist_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "As a user I can" do
-  it "edit an aritsts info" do
+RSpec.feature "As a user I can" do
+  scenario "edit an aritsts info" do
     artist = Artist.create(name: 'Queen', image: "http://cps-static.rovicorp.com/3/JPG_250/MI0003/201/MI0003201089.jpg?partner=allrovi.com")
     visit "/artists/#{artist.id}"
     click_on('Edit')
-    
-    expect(current_path).to eq(edit_artist_path)
+    artist_image_path = 'http://i.imgur.com/wGBi2Sq.gif'
+    expect(current_path).to eq(edit_artist_path(artist))
     
     fill_in('Name', with: 'Rick Astley')
-    fill_in('Image', with: 'http://i.imgur.com/wGBi2Sq.gif')
+    fill_in('Image', with: artist_image_path)
+    click_on('Submit')
     
     expect(current_path).to eq(artist_path(artist))
     expect(page).to have_content('Rick Astley')
-    expect(page).to have_("img[src=\"#{artist_image_path}\"]")
+    expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
     
   end
 end
